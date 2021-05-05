3f09083b43a506b7d4992f82a6f9bab9629afd0c
Lucas Johnson <lucas.johnson02@outlook.com>
2021-05-05 18:42:40 +0100

    started lab 5

---
+++ neit-2021-johnson/C#/Johnson_Lab5/Johnson_Lab5/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CommonClass;
+namespace Johnson_Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ neit-2021-johnson/C#/Johnson_Lab5/Johnson_Lab5/PersonV2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClass
+{
+    class PersonV2:Person
+    {
+        private String cellPhone;
+        private String instagramURL;
+        
+        public String CellPhone
+        {
+            get
+            {
+                return cellPhone;
+            }
+            set
+            {
+                cellPhone = value;
+            }
+        }
+        public String InstagramURL
+        {
+            get
+            {
+                return instagramURL;
+            }
+            set
+            {
+                instagramURL = value;
+            }
+        }
+    }
+}
