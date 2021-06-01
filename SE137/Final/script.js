80b9c23bb11dee790aab8eec7c7cb702dc9acf60
JohnsonL104 <lucas.johnson02@outlook.com>
2021-05-31 22:05:50 -0400

    finished final

--- SE137/Final/script.js
+++ SE137/Final/script.js
@@ -1,6 +1,24 @@
-var title = document.querySelector("#title")
+var img = document.querySelector(".jumbotron");
+var ig = document.querySelector(".fa-instagram-square")
+var twitter = document.querySelector(".fa-twitter-square")
+var github = document.querySelector(".fa-github-square")
 
 
-title.addEventListener("click", function(){
-    document.querySelector(".contentContainer").classList.toggle("shrunk")
+
+ig.addEventListener("click", function(){
+    window.open("https://instagram.com/", '_blank')
 })
+twitter.addEventListener("click", function(){
+    window.open("https://twitter.com/", '_blank');
+})
+github.addEventListener("click", function(){
+    window.open("https://github.com/JohnsonL104/neit-2021-johnson", '_blank');
+})
+img.addEventListener("click", function(){
+    window.open("featuredProject.html", '_self');
+})
+
+
+
+// things to do:
+// 1. make pictures clickable
