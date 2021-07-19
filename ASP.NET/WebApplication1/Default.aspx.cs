7903300cda0b75bcb0852ad1e5fa27abed848bce
JohnsonL104 <lucas.johnson02@outlook.com>
2021-07-19 16:29:07 -0400

    ASP shit

--- ASP.NET/WebApplication1/Default.aspx.cs
+++ ASP.NET/WebApplication1/Default.aspx.cs
@@ -13,5 +13,61 @@ namespace WebApplication1
         {
 
         }
+
+        protected void numBtnClick(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            txtLCD.Text += temp.Text;
+        }
+        protected void opBtnClick(object sender, EventArgs e)
+        {
+
+            if (Session["op"] == null || Session["op"].ToString() == "" ) 
+            {
+                Button temp = (Button)sender;
+                Session["op"] = temp.Text;
+                Session["num1"] = txtLCD.Text;
+                txtLCD.Text = "";
+            }
+            else
+            {
+                Button temp = (Button)sender;
+                Session["op"] = temp.Text;
+                txtLCD.Text = performMath().ToString();
+                Session["num1"] = txtLCD.Text;
+
+            }
+        }
+        protected void equalsBtnClick(object sender, EventArgs e)
+        {
+            txtLCD.Text = performMath().ToString();
+        }
+        protected void clsBtnClick(object sender, EventArgs e)
+        {
+            Session["op"] = "";
+            Session["num1"] = "";
+            txtLCD.Text = "";
+        }
+        private Double performMath()
+        {
+            Double result;
+            if(Session["op"].ToString() == "+")
+            {
+                return Double.Parse(Session["num1"].ToString()) + Double.Parse(txtLCD.Text);
+            }
+            else if (Session["op"].ToString() == "-")
+            {
+                return Double.Parse(Session["num1"].ToString()) - Double.Parse(txtLCD.Text);
+            }
+            else if (Session["op"].ToString() == "*")
+            {
+                return Double.Parse(Session["num1"].ToString()) * Double.Parse(txtLCD.Text);
+            }
+            else if (Session["op"].ToString() == "/")
+            {
+                return Double.Parse(Session["num1"].ToString()) / Double.Parse(txtLCD.Text);
+            }
+            return 0;
+        }
     }
 }
