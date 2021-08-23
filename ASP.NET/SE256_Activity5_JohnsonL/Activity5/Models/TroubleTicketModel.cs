b505012c2f6aaa5a94c6ed10c981115fb4a3e245
Lucas Johnson <lucas.johnson02@outlook.com>
2021-08-23 12:54:48 -0400

    did activity 5 asp.net

---
+++ ASP.NET/SE256_Activity5_JohnsonL/Activity5/Models/TroubleTicketModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity5.Models
+{
+    public class TroubleTicketModel
+    {
+        public int TicketID { get; set; }
+
+        public String TicketTitle { get; set; }
+
+        public String TicketDesc { get; set; }
+
+        public String Category { get; set; }
+
+        public String ReportingEmail { get; set; }
+
+        public DateTime OrigDate { get; set; }
+
+        public DateTime CloseDate { get; set; }
+
+        public String ResponderNotes { get; set; }
+
+        public String ResponderEmail { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
