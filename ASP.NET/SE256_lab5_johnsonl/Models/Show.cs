db7d74595da12b26980221ab682e49532c40a8ce
JohnsonL104 <lucas.johnson02@outlook.com>
2021-09-01 11:41:24 -0400

    idk it's been a minute since i pushed

---
+++ ASP.NET/SE256_lab5_johnsonl/Models/Show.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SE256_lab5_johnsonl.Models
+{
+    public class Show
+    {
+        public int ShowID { get; set; }
+
+        public String Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int EpisodeCount { get; set; }
+    }
+}
